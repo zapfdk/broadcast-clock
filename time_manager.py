6ee951f8714140d533ee96f6b7114f3462380bcd
Dominik Zapf <dominik.zapf@tu-ilmenau.de>
2016-11-15 23:56:05 +0100

    Functionable time_manager

--- time_manager.py
+++ time_manager.py
@@ -12,7 +12,6 @@ def get_time_from_txt(filename):
     try: 
         with open("end_time.txt", "r") as f:
             end_time = f.readline().strip()
-            print(end_time)
             end_time = time.strptime(end_time, "%d.%m.%Y.%H:%M:%S")
             return end_time
     except:
@@ -32,17 +31,12 @@ def save_time_to_txt(end_time):
             time.sleep(0.3)
         
     
-def add_secs_to_end_time(seconds):
+def add_secs_to_end_time(delta_seconds):
     current_end_time = get_time_from_txt("end_time.txt")
+    current_end_seconds = time.mktime(current_end_time)
+    new_end_seconds = current_end_seconds + delta_seconds
+    save_time_to_txt(time.localtime(new_end_seconds))
     
-    
-current_end_time = get_time_from_txt("end_time.txt")
-print(current_end_time)
-
-current_end_time.tm_sec += 10
-
-print(current_end_time)
-
-    
-
-            
+print(get_time_from_txt("end_time.txt"))
+add_secs_to_end_time(-30)      
+print(get_time_from_txt("end_time.txt"))
