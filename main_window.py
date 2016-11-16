770351f7e18db53fcc32d23993061be44c47b408
Dominik Zapf <dominik.zapf@tu-ilmenau.de>
2016-11-16 02:02:51 +0100

    Made font size changeable

--- main_window.py
+++ main_window.py
@@ -17,8 +17,10 @@ class MainWindow(tk.Tk):
         
         self.end_time_file = "end_time.txt"
         
+        self.countdown_font_size = 300
         
-        self.remaining_label = tk.Label(self, text="", width=1,height=1, font=(None,300), fg="red", bg="black")        
+        
+        self.remaining_label = tk.Label(self, text="", width=1,height=1, font=(None,self.countdown_font_size), fg="red", bg="black")        
         self.remaining_label.pack(fill=tk.BOTH, expand=1)
         
         self.blink_on = False
@@ -32,7 +34,7 @@ class MainWindow(tk.Tk):
         self.remaining = tm.get_remaining_time(self.end_time_file)
         
         if self.remaining.days < 0:
-            self.remaining_label.configure(text="Zeit abgelaufen!")
+            self.remaining_label.configure(text="0:00:00\nZeit abgelaufen!")
         else:
             self.remaining_label.configure(text=str(timedelta(seconds=self.remaining.seconds)))
             
@@ -57,10 +59,20 @@ class MainWindow(tk.Tk):
         
     def sub_seconds(self, event=None):
         tm.add_secs_to_end_time(-60)
+    
+    def reduce_font_size(self, event=None):
+        self.countdown_font_size -= 5
+        self.remaining_label.configure(font=(None,self.countdown_font_size))
+        
+    def increase_font_size(self, event=None):
+        self.countdown_font_size += 5
+        self.remaining_label.configure(font=(None,self.countdown_font_size))
         
     def bind_keys(self):
         self.bind("+", self.add_seconds)
-        self.bind("-", self.sub_seconds) 
+        self.bind("-", self.sub_seconds)
+        self.bind("s", self.reduce_font_size)
+        self.bind("w", self.increase_font_size)
 
 if __name__ == "__main__":
     app = MainWindow()
