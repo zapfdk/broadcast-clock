a552a75d82e17d368dd279c6426b82a1abae4a62
Dominik Zapf <dominik.zapf@tu-ilmenau.de>
2016-11-16 01:12:40 +0100

    First kind of functionable version. Countdown is working. Has to be configured over file

--- main_window.py
+++ main_window.py
@@ -16,20 +16,35 @@ class MainWindow(tk.Tk):
         
         self.end_time_file = "end_time.txt"
         
-        self.remaining_label = tk.Label(self, text="", width=10)
-#        sel
-        self.remaining_label.grid(row=0)
+        self.remaining_label = tk.Label(self, text="", width=10, font=(None,300), fg="red")        
+        self.remaining_label.pack()
+        
+        self.blink_on = False
+        
         
         self.remaining = tm.get_remaining_time(self.end_time_file)
         self.countdown()
 
     def countdown(self):       
         if self.remaining.days < 0:
-            self.remaining_label.configure(text="time's up!")
+            self.remaining_label.configure(text="Zeit abgelaufen!")
         else:
             self.remaining = tm.get_remaining_time(self.end_time_file)
             self.remaining_label.configure(text=str(timedelta(seconds=self.remaining.seconds)))
-            self.after(1000, self.countdown)            
+            
+            #Attention please!
+            if (self.remaining.seconds < 10):
+                self.toggle_color(self.remaining_label)
+            
+            self.after(1000, self.countdown)
+            
+    def toggle_color(self, label):
+        if self.blink_on:
+            label.configure(fg="white", bg="red")
+            self.blink_on = False
+        else:
+            label.configure(fg="red", bg="white")
+            self.blink_on = True
 
 if __name__ == "__main__":
     app = MainWindow()
