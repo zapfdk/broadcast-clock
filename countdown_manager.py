b0faaaab67ca02b402515c20009e2183b1a6e3fa
Dominik Zapf <dominik.zapf@tu-ilmenau.de>
2016-11-19 22:10:52 +0100

    Changed layout of main window. Added check if script is already running

--- countdown_manager.py
+++ countdown_manager.py
@@ -6,21 +6,33 @@ Created on Thu Nov 17 14:18:18 2016
 """
 
 import sys
+import os
 
 import main_window
 import time_manager as tm
-
+    
 def start_countdown_instance():
     countdown_instance = main_window.MainWindow()
     countdown_instance.mainloop()
-
-if __name__ == "__main__":
-    arguments = sys.argv
     
-    if sys.argv[1] == "start":
-        start_countdown_instance()
+def check_arguments(arguments):       
+    if arguments[1] == "-start":
+        pid = str(os.getpid())
+        pidfile = "broadcast_countdown.pid"
+        
+        if os.path.isfile(pidfile):
+            print("%s process is already running." %pidfile)
+            sys.exit()
+            
+        try:
+            with open(pidfile, "w") as f:
+                f.write(pid) 
+            start_countdown_instance()
+        finally:
+            if os.path.exists("broadcast_countdown.pid"):
+                os.remove("broadcast_countdown.pid")
     
-    if "-a"in arguments or "-s" in arguments:
+    if "-a" in arguments or "-s" in arguments:
         delta_seconds = 0
             
         if sys.argv[1] == "-a":
@@ -28,7 +40,21 @@ if __name__ == "__main__":
         elif sys.argv[1] == "-s":
             delta_seconds = -int(sys.argv[2])
         
-        tm.add_secs_to_end_time(delta_seconds)
+        tm.add_secs_to_end_time(delta_seconds)    
+
+if __name__ == "__main__":
+    arguments = sys.argv    
+    
+    if len(arguments) <= 1:
+        print("Please use command line arguments to do something:")
+        print("\tUse -start to begin the countdown")
+        print("\tUse -a <seconds> or -s <seconds> to add or subtract seconds to the countdown\n")
+        sys.exit()
+    else:
+        check_arguments(arguments)
+        
+
+            
         
         
     
--- main_window.py
+++ main_window.py
@@ -6,25 +6,46 @@ Created on Tue Nov 15 22:29:46 2016
 """
 
 import tkinter as tk
+import tkinter.font
 import time_manager as tm
 from datetime import datetime, timedelta
+
+import platform
 
 class MainWindow(tk.Tk):
     def __init__(self):
         tk.Tk.__init__(self)
         
         self.screen_width, self.screen_height = self.winfo_screenwidth(), self.winfo_screenheight()
-        self.geometry("%dx%d+0+0" % (self.screen_width, self.screen_height))        
-        
-        self.configure(bg="black")
-        
+        self.geometry("%dx%d+0+0" % (self.screen_width, self.screen_height))
+        self.configure(bg="black") 
         self.end_time_file = "end_time.txt"
         
-        self.countdown_font_size = 300
+        self.lift()
         
+        if "windows" in platform.system():
+            self.attributes('-topmost', True)
+            self.attributes('-topmost', False)       
+            self.state("zoomed")
+            
+        first_row_weight = 0.7
+        second_row_weight = 1-first_row_weight
+            
+        self.grid_rowconfigure(0,minsize=int(first_row_weight*self.screen_height))
+        self.grid_rowconfigure(2,minsize=int(second_row_weight*self.screen_height))
+        self.grid_columnconfigure(0,minsize=int(self.screen_width))
         
-        self.remaining_label = tk.Label(self, text="", width=1,height=1, font=(None,self.countdown_font_size), fg="red", bg="black")        
-        self.remaining_label.pack(fill=tk.BOTH, expand=1)
+        font_helv_countdown = tkinter.font.Font(family="Arial",size=int(self.screen_height/3),weight="bold")
+        font_helv_countdown_name = tkinter.font.Font(family="Arial",size=int(self.screen_height*0.15))
+        
+        self.remaining_label = tk.Label(self, text="", width=1,height=1, font=font_helv_countdown, fg="red", bg="black")        
+        self.remaining_label.grid(row = 0, sticky=tk.W+tk.E+tk.N+tk.S)
+        
+        self.separator = tk.Frame(self,height=3,bg="red")
+        self.separator.grid(row=1,sticky="ew")
+        
+        self.countdown_to_name_label = tk.Label(self, text="-> MAZ",font=font_helv_countdown_name,fg="red",bg="black")
+        self.countdown_to_name_label.grid(row = 2, sticky=tk.W+tk.E+tk.N)
         
         self.blink_on = False
         
@@ -58,7 +79,6 @@ class MainWindow(tk.Tk):
             self.blink_on = True
             
     def add_seconds(self, event=None):
-        print("hello")
         tm.add_secs_to_end_time(60)
         
     def sub_seconds(self, event=None):
